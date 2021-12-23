8f95ff3789182080ace1b27a6691a7f87be08ad2
sangamesh <sangamesh.hamje@gmail.com>
2021-12-23 14:46:01 +0530

    Exptiom modified

---
+++ src/com/atipune/exceptions/M1.java
@@ -0,0 +1,19 @@
+package com.atipune.exceptions;
+
+public class M1 {
+
+	public static void main(String[] args) {
+		try {
+			
+		int a=10,b=0,c;
+		c=a/b;
+		System.out.println("c"+c);
+		}
+		catch(arthmeticexpression.e)
+		{
+			System.out.println(e);
+		}
+	}
+}
+
+
